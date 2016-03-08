fe2141f1ff269a5dd9312dd77183cfc2f59b6df9
bunny9 <bunny9@bk.ru>
2016-03-08 20:22:45 +0300

    тянутся новости с сайта

--- index.js
+++ index.js
@@ -6,87 +6,81 @@ var bodyParser = require('body-parser');
 var template = require('consolidate').handlebars;
 var request = require('request');
 var cheerio = require('cheerio');
+var cookieParser = require('cookie-parser');
+
 
 var app = express();
+
+var server = app.listen(8000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Server was running on: ", host, port);
+});
+
+var newsArr = {
+    'politika': 'http://www.livekuban.ru/news/politika/',
+    'ekonomika': 'http://www.livekuban.ru/news/ekonomika/',
+    'kultura': 'http://www.livekuban.ru/news/kultura/',
+    'proisshestviya': 'http://www.livekuban.ru/news/proisshestviya/',
+    'sport': 'http://www.livekuban.ru/news/sport/'
+
+};
 
 app.engine('hbs', template);
 app.set('view engine', 'hbs');
 app.set('views', __dirname + "/views");
-
 app.use(bodyParser.urlencoded({extended: false}));
-
 app.use(bodyParser.json());
+app.use(cookieParser())
+
+
 app.get('/', function (req, res) {
     res.render('index');
 });
 
 app.post("/", function (req, res) {
     var arr = req.body;
-    var result = "";
-    switch (arr.resurs) {
-        case 'mail':
-            result = getNewsMail(arr.razdel);
-            break;
-        case 'yandex':
-            result = getNewsYandex(arr.razdel);
-            break;
-        case 'ria':
-            result = getNewsRia(arr.razdel);
-            break;
+    var body = {};
+    if (req.cookies.razdel)
+        for (var key in newsArr) {
+            if (key == req.cookies.razdel) {
+                body = {key: true};
+            }
+        }
+    if (req.cookie) {
+        count = "c" + req.cookie.count;
+        body = {count: true};
     }
-    res.render('index', {result: result})
-})
+    //console.log(req.cookies);
+    res.cookie('razdel', arr.razdel);
+    res.cookie('count', arr.count);
+    getNews(arr.razdel, arr.count, function (result) {
+        res.render('index', {result: result,body:body})
+    });
 
-var server = app.listen(8000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log("Server was running on: ", host, port);
 
 });
-var newsArr = {
-    'mail': {
-        'politics': 'https://news.mail.ru/politics/',
-        'economy': 'https://news.mail.ru/economics/',
-        'sport': 'https://sport.mail.ru/'
-    },
-    'yandex': {
-        'politics': 'https://news.yandex.ru/politics.html',
-        'economy': 'https://news.yandex.ru/business.html',
-        'sport': 'https://news.yandex.ru/sport.html'
-    },
-    ria: {
-        'politics': 'http://ria.ru/politics/',
-        'economy': 'http://ria.ru/economy/',
-        'sport': 'http://ria.ru/sport/'
-    }
-}
-function getNewsMail(razdel) {
-    var result = ""
-    request(newsArr.mail[razdel], function (error, response, html) {
-        result = error;
+
+
+function getNews(razdel, count, callback) {
+    var news = {};
+    request(newsArr[razdel], function (error, response, html) {
         if (!error && response.statusCode == 200) {
             var $ = cheerio.load(html);
-            var news={}
-            $('.cell').each(function () {
-                console.log("here3");
-                if(!$(this).hasClass('cell_photo')) {
-                    var oneNew={};
-                    oneNew['title'] = $(this).find('.newsitem__title-inner').text();
-                    oneNew['text']=$(this).find('.newsitem__text').text()
-                }
-                news[news.length]=oneNew;
+            news = {};
+            $('.node-type-interview').each(function () {
+                var oneNew = {};
+                oneNew['title'] = $(this).find(".field-title div a").text();
+                oneNew['date'] = $(this).find('.date').text();
+                oneNew['description'] = $(this).find(".field-teaser div").text();
+                var object_length = Object.keys(news).length;
+                if (object_length < count)
+                    news['new' + object_length] = oneNew;
+
             });
-            result=news;
+            callback(news);
         }
     });
-    return result;
 }
 
-function getNewsYandex(razdel) {
-    return "Вы выбрали " + razdel;
-}
 
-function getNewsRia(razdel) {
-    return "Вы выбрали " + razdel;
-}
